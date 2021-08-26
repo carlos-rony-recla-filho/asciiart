a679c5618e9e93c40eb22551cc6158f04e7c3544
Carlos Rony <carlosrecla03@gmail.com>
2021-08-26 17:03:47 -0300

    final da tarefa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace asciiart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("");
+            Console.WriteLine("  <_>");
+            Console.WriteLine(" __|___ ");
+            Console.WriteLine(" |~ ~ |");
+            Console.WriteLine("(|()()|)");
+            Console.WriteLine(" |----| ");
+            Console.WriteLine(" |____|");
+            Console.WriteLine();
+            Console.WriteLine("OLÁ USUÁRIO");
+        }
+    }
+}
